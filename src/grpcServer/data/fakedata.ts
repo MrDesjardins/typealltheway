a9d05fd86daf71c2574ae92a22afd64ff80d1491
Patrick Desjardins <pdesjardins3@gatech.edu>
2020-07-04 20:35:27 -0700

    First commit. gRPC server is running

---
+++ src/grpcServer/data/fakedata.ts
@@ -0,0 +1,30 @@
+export const profiles: any = {
+  "1": {
+    id: 1,
+    firstname: "Patrick",
+    lastname: "Desjardins",
+    active: true,
+    friendIds: [2, 3, 4]
+  },
+  "2": {
+    id: 1,
+    firstname: "Melodie",
+    lastname: "Savaria",
+    active: true,
+    friendIds: [1]
+  },
+  "3": {
+    id: 1,
+    firstname: "Alicia",
+    lastname: "Desjardins",
+    active: false,
+    friendIds: [1, 2]
+  },
+  "4": {
+    id: 1,
+    firstname: "Jacob",
+    lastname: "Desjardins",
+    active: true,
+    friendIds: [2, 3]
+  },
+}
---
+++ src/grpcServer/server.ts
@@ -0,0 +1,42 @@
+const PROTO_PATH = './src/grpcServer/protobuf/profile.proto';
+import grpc from 'grpc';
+import { loadSync } from '@grpc/proto-loader';
+import { profiles } from './data/fakedata';
+
+const packageDefinition = loadSync(
+  PROTO_PATH,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  });
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+const profileservice = protoDescriptor.profileservice;
+// console.log("Description Package:", profileservice);
+
+function getProfile(call: any): any {
+  const request = call.request;
+  const id = request.id;
+  return profiles[id];
+}
+function getProfiles(call: any): any {
+  const request = call.request;
+  const ids = request.ids;
+  return Object.values(profiles).filter((d: any) => ids.indexOf(d.id) >= 0);
+}
+function listProfiles(): any {
+  return Object.values(profiles);
+}
+
+const server = new grpc.Server();
+server.addProtoService((profileservice as any).ProfileService.service, {
+  getprofile: getProfile,
+  getprofiles: getProfiles,
+  listprofiles: listProfiles,
+
+});
+
+server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+server.start();
